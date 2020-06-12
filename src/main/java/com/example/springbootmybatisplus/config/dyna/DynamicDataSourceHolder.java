8d88c4395c6a96aec718fc8eebcd5bdb4e6b378d
ZongjiesWu <2012293155@qq.com>
2020-06-13 05:36:59 +0800

    提交读写分离代码

---
+++ src/main/java/com/example/springbootmybatisplus/config/dyna/DynamicDataSourceHolder.java
@@ -0,0 +1,29 @@
+package com.example.springbootmybatisplus.config.dyna;
+
+
+
+/**
+ * @author lulu
+ * @Date 2019/6/8 12:14
+ */
+public class DynamicDataSourceHolder {
+    private static ThreadLocal<String> contextHolder = new ThreadLocal<>();
+    public static final String DB_MASTER = "master";
+    public static final String DB_SLAVE = "slave";
+
+    public static String getDbType() {
+        String db = contextHolder.get();
+        if (db == null) {
+            db = DB_MASTER;
+        }
+        return db;
+    }
+
+    public static void setDBType(String str) {
+        contextHolder.set(str);
+    }
+
+    public static void clearDbType() {
+        contextHolder.remove();
+    }
+}
